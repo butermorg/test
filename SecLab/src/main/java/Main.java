07453c52c2c9fc827512f78631fe788466e0c83b
Ps1h0 <platonov.liceum@mail.ru>
2021-02-12 12:56:18 +0800

    SEC-1 Test4

--- SecLab/src/main/java/Main.java
+++ SecLab/src/main/java/Main.java
@@ -4,7 +4,7 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("GG");
         System.out.println("WP");
-        System.out.println("sooqa");
+        System.out.println("fg");
     }
 
 }
