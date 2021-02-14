7106aa39a921f961d0bb01e3eefd74bb7a9cb330
Ps1h0 <platonov.liceum@mail.ru>
2021-02-14 22:11:50 +0800

    SEC-5 Тестовая задача

--- SecLab/src/main/java/Main.java
+++ SecLab/src/main/java/Main.java
@@ -1,11 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("GG");
-        System.out.println("WP");
-        System.out.println("fg");
-        System.out.println("SEX?");
+        
     }
 
 }
