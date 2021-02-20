16b786c997e999f9ec34e6fdfbf6ccbeb078f204
RomOleg <71003372+RomOleg@users.noreply.github.com>
2021-02-20 13:03:09 +0800

    html 0.2

---
+++ SecLab/src/main/js/index.js
@@ -0,0 +1,8 @@
+    /* Установите ширину боковой навигации до 250 пикселей */
+function openNav() {
+    $('#mySidenav').css('width', '250px');
+}
+    /* Установите ширину боковой навигации на 0 */
+function closeNav() {
+    $("#mySidenav").css('width', '0px');
+}
